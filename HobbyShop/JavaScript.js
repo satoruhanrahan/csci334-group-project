0157d5a0c00ba265dbd0e9503a64d574b5430e85
GrassMonk <GrassMonk@users.noreply.github.com>
2019-04-10 09:42:40 +1000

    Pushed Nam's code to GitHub
    Nam sent me her code, and I pushed it to the repository. This is because she is working on university computers, and cannot install github desktop on them.

---
+++ HobbyShop/JavaScript.js
@@ -0,0 +1,45 @@
+﻿// display all sale items
+function getInventoryName() {
+    Service1.GetInventoryName(onGetInventoryName);
+}
+
+function onGetInventoryName(result) {
+    var names = JSON.parse(result);
+    displayInventoryName(names);
+}
+
+
+function displayInventoryName(names) {
+    var contents = document.getElementById("pageContents");
+    
+    var count = 0;
+    for (var i = names.length - 1; i >= 0; i--) {
+        count++;
+        
+        var node = document.createElement("pageContents");                 
+        var textnode = document.createTextNode(names[i] + "<br>");
+        
+        node.appendChild(textnode);                              
+        document.getElementById("test").appendChild(node);
+    }
+}
+
+//Add new item to model table
+
+
+function addNewItemModel() {
+    var name = document.getElementById("modelName").value;
+    var type = document.getElementById("modelType").value;
+    var sbjarea = document.getElementById("modelArea").value;
+    var price = document.getElementById("modelPrice").value;
+    var des = document.getElementById("modelDes").value;
+    var avail = document.getElementById("modelAvail").value;
+   
+    Service1.AddNewModel(name, type, sbjarea, price, des, avail, onInputNewModel );
+}
+
+function onInputNewModel(result) {
+    if (result === "") {
+        document.getElementById("results").innerHTML = "Successfully added to the database";
+    }
+}
---
+++ HobbyShop/Run.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Run.aspx.cs" Inherits="HobbyShop.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Store Inventory</title>
+    <script type="text/javascript" src="JavaScript.js"></script>
+</head>
+<body>
+        <div>
+             DISPLAY NAME OF ALL ITEMS ON SALE <br />
+            <input type="button" class="inputButton" value="Display Store Inventory" onclick="getInventoryName();" />
+        </div>
+           
+         <div id="test"></div>
+    <br /><br />
+           ADD NEW MODEL <br />
+           Name:  <input type="text"  id="modelName"/> <br />
+           Type:  <input type="text"   id="modelType" /> <br />
+           Subject area:  <input type="text"   id="modelArea" /> <br />
+           Current retail price:  <input type="text"  id="modelPrice"/> <br />
+           Description:  <input type="text" id="modelDes"/> <br />
+           Availability:  <input type="text"  id="modelAvail"/> <br />
+           <input type="button" value="Add New Item to Model" onclick="addNewItemModel();" />
+
+    <div id="results"></div>
+    <form id="form1" runat="server">
+        <asp:scriptmanager runat="server">
+            <Services>
+               <asp:ServiceReference Path="~/Service1.svc" />
+            </Services>
+        </asp:scriptmanager>
+    </form>
+</body>
+</html>
