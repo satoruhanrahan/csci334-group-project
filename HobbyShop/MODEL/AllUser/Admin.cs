edf88cc38c707aaeafd4459e581072cc250a5c69
hoainamnguyen1998 <49138992+hoainamnguyen1998@users.noreply.github.com>
2019-05-25 16:11:15 +1000

    refactor

--- HobbyShop/MODEL/AllUser/Admin.cs
+++ HobbyShop/MODEL/AllUser/Admin.cs
@@ -8,27 +8,10 @@ namespace HobbyShop.CLASS
     class Admin : SystemUser
     {
         private readonly string userType;
-        private string username;
-        private string password;
-        private DateTime? lastLogged;
-        private string firstName;
-        private string lastName;
-        private int ID;
-
-        public override int Id { get { return ID; } set { ID = value; } }
         public override string UserType { get { return userType; }  }
-        public override string UserName { get { return username; } set { username = value; } }
-        public override string PassWord { get { return password; } set { password = value; } }
-        public override DateTime? LastLogged { get { return lastLogged; } set { lastLogged = value; } }
-        public override string FirstName { get { return firstName; } set { firstName = value; } }
-        public override string LastName { get { return lastName; } set { lastName = value; } }
-        public Admin() { }
-        public Admin(string username, string password, string firstName, string lastName)
+
+        public Admin(string username, string password, string firstName, string lastName) : base(username, password, firstName, lastName)
         {
-            this.username = username;
-            this.password = password;
-            this.firstName = firstName;
-            this.lastName = lastName;
             this.userType = "admin";
         }
     }
