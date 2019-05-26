58c3ef00da12dcdfbff5c52f3be253934aa0511b
GrassMonk <GrassMonk@users.noreply.github.com>
2019-05-26 22:56:35 +1000

    Store UI tweaks

--- HobbyShop/MODEL/Store.cs
+++ HobbyShop/MODEL/Store.cs
@@ -80,7 +80,7 @@ namespace HobbyShop.CLASS
                         {
                             string itemName = Convert.ToString(itemReader["Name"]);
                             int stockCount = Convert.ToInt32(itemReader["StockCount"]);
-                            int location = Convert.ToInt32(itemReader["LocationInShop"]);
+                            int location = Convert.ToInt32(itemReader["LocationInStore"]);
                             DateTime firstDate = Convert.ToDateTime(itemReader["FirstStockDate"]);
                             StoreInventory item = new StoreInventory(itemName, stockCount, location, firstDate);
                             itemList.Add(item);
